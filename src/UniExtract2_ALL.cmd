68cd4c3ca4927f2373d3e821fb54e0b8e3facd84
passiontolearn <adam.be.edge@gmail.com>
2017-11-19 23:46:35 +0200

    Update UniExtract2_ALL.cmd

--- src/UniExtract2_ALL.cmd
+++ src/UniExtract2_ALL.cmd
@@ -4,7 +4,7 @@
 set CurrentPath=%~dp0
 set UniExtract="D:\__Tools\win\UniExtract2\UniExtract.exe"
 
-FOR %%f in (*.exe .msi) DO ( 
+FOR %%f in (*.exe *.msi) DO ( 
 	REM Using a temp Folder ("Extracted") to workaround a UniExtract problem with folder names that end with ".exe" 
 	REM among other odd behaviors of trying to Automate UniExtract.exe in a bat script (!)
 	%UniExtract% %%f %CurrentPath%\Extracted\%%f
