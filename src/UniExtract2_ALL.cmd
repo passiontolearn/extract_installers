852a7f137b27711f5f8f568228bfdb3b2c358395
passiontolearn <adam.be.edge@gmail.com>
2017-11-21 02:38:14 +0200

    Update UniExtract2_ALL.cmd
    Fix: handle file names with spaces + added "/silent" switch to suppress (most) prompts.

--- src/UniExtract2_ALL.cmd
+++ src/UniExtract2_ALL.cmd
@@ -1,12 +1,12 @@
-:: Requires UniExtract2
-:: see: https://github.com/Bioruebe/UniExtract2/releases
+::@echo off &cls
+::SETLOCAL EnableDelayedExpansion
 
 set CurrentPath=%~dp0
 set UniExtract="D:\__Tools\win\UniExtract2\UniExtract.exe"
 
 FOR %%f in (*.exe *.msi) DO ( 
-	REM Using a temp Folder ("Extracted") to workaround a UniExtract problem with folder names that end with ".exe" 
+	REM Using a temp Folder ("Extracted") to workaround a UniExtract problem with foldernames that end with ".exe" 
 	REM among other odd behaviors of trying to Automate UniExtract.exe in a bat script (!)
-	%UniExtract% %%f %CurrentPath%\Extracted\%%f
+	%UniExtract% "%%f" %CurrentPath%\Extracted\"%%f" /silent
 )
 PAUSE
